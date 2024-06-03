4da9e31084f40129139dd50459eb4981d3e8526b
LIM YAN XIN <117059166+LYX0519@users.noreply.github.com>
2024-06-03 21:50:52 +0800

    added route for profile page

--- app.js
+++ app.js
@@ -4,29 +4,21 @@ const mongoose = require('mongoose');
 const path = require('path');
 const dotenv = require('dotenv');
 const multer = require('multer');
-
-const routes = require('./routes/index');
-const http = require('http');
-const socketio = require('socket.io');
-const Chat = require('./models/chats');
-
+const routes = require('./routes/index'); //Import index routes
 const signup_routes = require('./routes/signup'); // Import signup routes
 const login_routes = require('./routes/login'); // Import login routes
+const profile_routes = require('./routes/profile'); // Include profile routes
 const nav_routes = require('./routes/nav');
-//const profile_routes = require('./routes/profile'); // Include profile routes
 
 
 dotenv.config();
 console.log('MONGO_URI:', process.env.MONGO_URI); 
 
 const app = express();
-const server = http.createServer(app);
-const io = socketio(server);
-
 const port = process.env.PORT || 3003;
 
 // Set up mongoose connection
-mongoose.connect(process.env.MONGO_URI, {useUnifiedTopology: true, useNewUrlParser: true })
+mongoose.connect(process.env.MONGO_URI, {useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log('MongoDB connection error:', err));
 
@@ -36,34 +28,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use('/', routes); // Use index routes
-app.use('/', nav_routes);
 app.use('/', signup_routes); // Use signup routes
 app.use('/', login_routes); // Use login routes
-//app.use('/', profile_routes); // Use profile routes
+app.use('/', profile_routes); // Use profile routes
+app.use('/', nav_routes);
 
-// Real-time chat functionality
-io.on('connection', (socket) => {
-    console.log('New WebSocket connection');
-
-    socket.on('chat message', async ({ username, message }) => {
-        try {
-            const name = socket.handshake.query.name; // Assume name is passed as a query param
-            const newChat = new Chat({
-                name: name, // Assuming 'name' is the name
-                username,
-                message,
-                timestamp: new Date()
-            });
-
-            await newChat.save();
-            io.emit('chat message', { username, message, timestamp: new Date() }); // Emit to all connected clients
-        } catch (error) {
-            console.error(error);
-        }
-    });
-});
-
-// Start the server
-server.listen(port, () => {
+app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
